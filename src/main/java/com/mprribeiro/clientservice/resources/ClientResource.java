b703abe2b40c10add9d1dacbc8ded17c95f109de
Marcos Ribeiro <mprribeiro1902@gmail.com>
2021-09-18 13:07:34 -0300

    FindAll paginated and insert

--- src/main/java/com/mprribeiro/clientservice/resources/ClientResource.java
+++ src/main/java/com/mprribeiro/clientservice/resources/ClientResource.java
@@ -1,11 +1,20 @@
 package com.mprribeiro.clientservice.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mprribeiro.clientservice.dto.ClientDTO;
 import com.mprribeiro.clientservice.services.ClientService;
@@ -21,5 +30,25 @@ public class ClientResource {
 	public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
 		var clientDTO = clientService.findById(id);
 		return ResponseEntity.ok(clientDTO);
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "10") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		
+		var clientsDTO = clientService.findAll(pageRequest);
+		return ResponseEntity.ok(clientsDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto) {
+		var clientDTO = clientService.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(clientDTO.getId()).toUri();
+		return ResponseEntity.created(uri).body(clientDTO);
 	}
 }
--- src/main/java/com/mprribeiro/clientservice/services/ClientService.java
+++ src/main/java/com/mprribeiro/clientservice/services/ClientService.java
@@ -1,10 +1,13 @@
 package com.mprribeiro.clientservice.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.mprribeiro.clientservice.dto.ClientDTO;
+import com.mprribeiro.clientservice.entities.Client;
 import com.mprribeiro.clientservice.repositories.ClientRepository;
 import com.mprribeiro.clientservice.services.exceptions.ResourceNotFoundException;
 
@@ -17,6 +20,24 @@ public class ClientService {
 	@Transactional(readOnly = true)
 	public ClientDTO findById(Long id) {
 		var client = clientRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Entity not found for id " + id));
+		return new ClientDTO(client);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<ClientDTO> findAll(PageRequest pageRequest) {
+		var clients = clientRepository.findAll(pageRequest);
+		return clients.map(client -> new ClientDTO(client));
+	}
+
+	@Transactional()
+	public ClientDTO insert(ClientDTO dto) {
+		var client = new Client();
+		client.setName(dto.getName());
+		client.setCpf(dto.getCpf());
+		client.setIncome(dto.getIncome());
+		client.setBirthDate(dto.getBirthDate());
+		client.setChildren(dto.getChildren());
+		client = clientRepository.save(client);
 		return new ClientDTO(client);
 	}
 }
